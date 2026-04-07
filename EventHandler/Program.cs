c4d6290ed61cba2f658884c24e60da3f699854f5
Nathanaells <nathanaelsinaga27@gmail.com>
2026-04-07 11:47:54 +0700

    learn event Handler

---
+++ EventHandler/Program.cs
@@ -0,0 +1,12 @@
+﻿//Event Handler
+
+class Program
+{
+    static void Main(string[] args){
+        
+
+    }
+    public delegate void PriceChangedHandler(decimal oldPrice, decimal newPrice);
+
+}
+
---
+++ FuncAction/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+    Func<int, string> testFunc = (x) =>
+    {
+        if(x % 2 == 0)
+        {
+            return "Even";
+        }
+        else
+        {
+            return "Odd";
+        }   
+    };   
+
+    Console.WriteLine(testFunc(5));  
+    }
+}
+
