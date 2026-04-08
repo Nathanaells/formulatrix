586ac3ee31bb83b83ac3f5e10ae757e0b24c3cd4
Nathanaells <nathanaelsinaga27@gmail.com>
2026-04-08 09:35:13 +0700

    event handler general pattern

--- EventHandler2/Program.cs
+++ EventHandler2/Program.cs
@@ -1,39 +1,69 @@
-﻿class Program
+﻿using System;
+
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        
+        Stock stock = new Stock("tes");
+        stock.PriceChanged += Stock_PriceChanged;
+
+        stock.Price = 27.10M;
+        stock.Price = 31.59M;
+        stock.Price = 43.35M;
+        stock.Price = 53.35M;
+
+        // stock.PriceChanged += Stock_PriceChanged;
+
+        void Stock_PriceChanged(object? sender, PriceChangedEventArgs e)
+        {
+            if (e.LastPrice > 0 && (e.NewPrice - e.LastPrice) / e.LastPrice > 0.1M)
+            {
+                // Console.WriteLine("Alert, 10% stock price Increase");
+                Console.WriteLine(e.NewPrice);
+            }
+        }
     }
 }
 
-public delegate void PriceChangedHandler(decimal oldPrice, decimal newPrice);
+public class PriceChangedEventArgs : EventArgs
+{
+    public readonly decimal LastPrice;
+    public readonly decimal NewPrice;
 
+    public PriceChangedEventArgs(decimal lastPrice, decimal newPrice)
+    {
+        this.LastPrice = lastPrice;
+        this.NewPrice = newPrice;
+    }
+}
 
 public class Stock
 {
-    string symbol;
-    decimal price;
-    
-    public Stock(string symbol) => this.symbol = symbol;
+    string mySymbol;
+    decimal myPrice;
 
-    public event PriceChangedHandler PriceChanged;
+    public Stock(string symbol)
+    {
+        this.mySymbol = symbol;
+    }
+
+    public event EventHandler<PriceChangedEventArgs>? PriceChanged;
+
+    protected virtual void OnPriceChanged(PriceChangedEventArgs e)
+    {
+        PriceChanged?.Invoke(this, e);
+    }
 
     public decimal Price
     {
-        get => price;
+        get => myPrice;
         set
         {
-            if (price == value) return;
-
-            decimal oldPrice = price;
-            price = value;
-
-            if(PriceChanged != null)
-            {
-                PriceChanged(oldPrice, price);
-                
-            }
-            
+            if (myPrice == value)
+                return;
+            decimal oldPrice = myPrice;
+            myPrice = value;
+            OnPriceChanged(new PriceChangedEventArgs(oldPrice, myPrice));
         }
     }
 }
--- MulticastDelegate/Program.cs
+++ MulticastDelegate/Program.cs
@@ -2,20 +2,19 @@
 {
     public static void Main(string[] args)
     {
-    Test test = new Test();
-    
-    Test.SomeDelegate d = test.SomeMethod;
-    d += test.SomeMethod2;
-    d.Invoke();
+        Test test = new Test();
+
+        Test.SomeDelegate d = test.SomeMethod;
+        d += test.SomeMethod2;
+        d.Invoke();
     }
 }
 
-
 class Test
 {
-   public delegate void SomeDelegate();
+    public delegate void SomeDelegate();
 
     public void SomeMethod() => Console.WriteLine("Method 1");
-    public void SomeMethod2() => Console.WriteLine("Method 2");
 
+    public void SomeMethod2() => Console.WriteLine("Method 2");
 }
