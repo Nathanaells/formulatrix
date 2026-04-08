5dfa69c70ce4a050525c3b6365516a9d49f541a1
Nathanaells <nathanaelsinaga27@gmail.com>
2026-04-08 10:38:45 +0700

    learn Enumerator

--- EnumeratonIterators/Program.cs
+++ EnumeratonIterators/Program.cs
@@ -1,7 +1,35 @@
 ﻿class Program
 {
+    static IEnumerable<int> Fibonacci(int count)
+    {
+        for (int i = 0, prevFib = 1, curFib = 1; i < count; i++)
+        {
+            yield return prevFib; // ini yang di return ke EvenNumbersOnly
+            int newFib = prevFib + curFib;
+            prevFib = curFib;
+            curFib = newFib;
+        }
+    }
+
+    static IEnumerable<int> EvenNumbersOnly(IEnumerable<int> sequence)
+    // EvenNumbersOnly nerima method IENUMERABLE
+    {
+        foreach (int x in sequence)
+        {
+            // Console.WriteLine(x + "<<<<<<<<<<<<<<<<<< ini ANGKA FIBONACCI");
+
+            if ((x % 2) == 0)
+            {
+                yield return x;
+            }
+        }
+    }
+
     static void Main(string[] args)
     {
-        
+        foreach (int a in EvenNumbersOnly(Fibonacci(10)))
+        {
+            Console.WriteLine(a + "INI YANG GENAP");
+        }
     }
 }
