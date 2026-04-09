dca343707621a9ef405c64f4ce5330989e772ae9
Nathanaells <nathanaelsinaga27@gmail.com>
2026-04-09 10:51:31 +0700

    learn: string and text handler

--- StringTextHandling/Program.cs
+++ StringTextHandling/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // string nullString = "";
+        // Console.WriteLine(nullString == null);
+        // Console.WriteLine(nullString == "");
+        // Console.WriteLine(string.IsNullOrEmpty(nullString));
+
+        // Console.WriteLine(string.Equals("FOO", "foo"));
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 50; i++)
+        {
+            sb.Append(i);
+        }
+
+        sb.Insert(0, "tes");
+
+        Console.WriteLine(sb.ToString());
+    }
+}
