5235b5fcbd2dd987e7cad9c0ac90007ec3da5ae9
Nathanaells <nathanaelsinaga27@gmail.com>
2026-04-09 10:24:53 +0700

    learn: new materi

---
+++ OperatorOverloading/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Proram
+{
+    public static void Main(string[] args)
+    {
+        Note B = new Note(2);
+        Note C = new Note(10);
+        Note A = B;
+        A += C;
+
+        Console.WriteLine(A.getValue());
+    }
+}
+
+public struct Note
+{
+    private int value;
+
+    public int getValue()
+    {
+        return value;
+    }
+
+    public Note(int semiTonesFromA)
+    {
+        value = semiTonesFromA;
+    }
+
+    public static Note operator +(Note x, Note semitones)
+    {
+        return new Note(x.value + semitones.value);
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
