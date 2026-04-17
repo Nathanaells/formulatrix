637f1d6515a24a8ee2cfb4c103cbf17d007e977c
Nathanaells <nathanaelsinaga27@gmail.com>
2026-04-17 14:54:26 +0700

    leran

---
+++ ConditionalCompilation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        LogDebug();
+    }
+
+    [Conditional("DEBUG")]
+    static void LogDebug()
+    {
+        Debug.WriteLine("This is a debug message.");
+        Console.WriteLine("Debug mode aktif");
+    }
+}
---
+++ WeakReferences/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        var sb = new StringBuilder("Hello, World!");
+        var weak1 = new WeakReference(sb);
+
+        Console.WriteLine(weak1.Target); // True
+
+        var weak2 = GetWeakRef();
+        GC.Collect();
+        Console.WriteLine(weak2.Target); // False
+
+        var sb2 = (StringBuilder)weak2.Target;
+
+        if (sb2 != null)
+            Console.WriteLine(sb2.ToString());
+        else
+            Console.WriteLine("Object has been collected");
+
+        WeakReference GetWeakRef() => new WeakReference(new StringBuilder("weak"));
+    }
+}
